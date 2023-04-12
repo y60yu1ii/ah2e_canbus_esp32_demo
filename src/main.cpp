d9bc2fd3e6e2523b9760a08eef9c71c0faed509f
yaoyu <yaoyuli@pqdusa.com>
2023-04-12 17:58:35 +0800

    1. add modbus

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,71 @@
 #include <ESPmDNS.h>
 #include <WiFi.h>
 #include <WiFiManager.h>
+// Modbus server include
+#include "ModbusServerWiFi.h"
+// #include "ModbusClientTCP.h"
 
 #define LED_PIN 13
 char ssid[32] = "ah2e_wifi";
 char mdns[32] = "ah2e";
 char password[64] = "123456789";
-
 WiFiManager wm;
+
+uint16_t port = 502;
+ModbusServerWiFi MBserver;
+int hearbeat = 0;
+// Server function to handle FC 0x03 and 0x04
+ModbusMessage FC0304(ModbusMessage request) {
+    ModbusMessage response;  // The Modbus message we are going to give back
+    uint16_t addr = 0;       // Start address
+    uint16_t words = 0;      // # of words requested
+    request.get(2, addr);    // read address from request
+    request.get(4, words);   // read # of words from request
+
+    // Address overflow?
+    if ((addr + words) > 20) {
+        // Yes - send respective error response
+        response.setError(request.getServerID(), request.getFunctionCode(), ILLEGAL_DATA_ADDRESS);
+    } else {
+        // Set up response
+        response.add(request.getServerID(), request.getFunctionCode(), (uint8_t)(words * 2));
+        // Request for FC 0x03?
+        if (request.getFunctionCode() == READ_HOLD_REGISTER) {
+            // Yes. Complete response
+            for (uint8_t i = 0; i < words; ++i) {
+                if (i == 0) {
+                    response.add((uint16_t)(hearbeat));
+                } else {
+                    response.add((uint16_t)(addr + i));
+                }
+            }
+        }
+        if (request.getFunctionCode() == READ_INPUT_REGISTER) {
+            // No, this is for FC 0x04. Response is random
+            for (uint8_t i = 0; i < words; ++i) {
+                // send increasing data values
+                // response.add((uint16_t)random(1, 65535));
+                if (i == 0) {
+                    response.add((uint16_t)(hearbeat + 1));
+                } else {
+                    response.add((uint16_t)random(1, 10));
+                }
+            }
+        }
+    }
+    // Send response back
+    return response;
+}
+
 void setup() {
     Serial.begin(115200);
     while (!Serial)
         ;
     Serial.println("Basic Demo - ESP32-WiFiManager");
     bool res;
-    wm.resetSettings();
+    // wm.resetSettings();
     res = wm.autoConnect(ssid, password);  // password protected ap
+
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, true);
 
@@ -37,8 +87,18 @@ void setup() {
         Serial.print(mdns);
         Serial.println(".local");
     }
+
+    // Set up TCP server to react on FCs 0x03 and 0x04
+    MBserver.registerWorker(1, READ_HOLD_REGISTER, &FC0304);
+    MBserver.registerWorker(1, READ_INPUT_REGISTER, &FC0304);
+    // Start server
+    MBserver.start(port, 2, 2000);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    hearbeat++;
+    if (hearbeat > 15) {
+        hearbeat = 0;
+    }
+    delay(100);
 }
